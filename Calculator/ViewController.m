0c725c5d19c7d9a937bc77e30b79e7807b04f649
Hui Chen <hui@ebrary.com>
2011-12-18 20:42:27 -0800

    more changes

--- Calculator/ViewController.m
+++ Calculator/ViewController.m
@@ -7,15 +7,32 @@
 //
 
 #import "ViewController.h"
+@interface ViewController()
+@property (nonatomic) BOOL userIsInMiddleOfEnteringANumber;
+@end
 
 @implementation ViewController
 @synthesize display = _display;
+@synthesize userIsInMiddleOfEnteringANumber = _userIsInMiddleOfEnteringANumber;
 @synthesize digit7 = _digit7;
 - (IBAction)digitPressed:(UIButton *)sender {
     NSLog(@"digit %@ is pressed.", sender.currentTitle);
-    self.display.text = sender.currentTitle;
+    NSString *digit = sender.currentTitle;
+    if (self.userIsInMiddleOfEnteringANumber){
+        self.display.text = [self.display.text stringByAppendingString:digit];
+        
+    }else{
+        self.display.text = digit;
+        self.userIsInMiddleOfEnteringANumber = YES;
+    }
 }
 
+- (IBAction)operandPressed:(UIButton *)sender {
+}
+
+
+- (IBAction)enterPressed {
+}
 
 - (void)viewDidUnload {
     [self setDigit7:nil];
